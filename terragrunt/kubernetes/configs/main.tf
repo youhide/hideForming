920ecbb165b62b948a8fd78c36c0baa3e3cc1490
youhide <youri@youhide.com.br>
2025-05-08 18:30:36 -0300

    feat(terraform): Add initial configuration files for Kubernetes setup

---
+++ terragrunt/kubernetes/configs/main.tf
@@ -0,0 +1,11 @@
+resource "kubernetes_node_taint" "master_taint" {
+  metadata {
+    name = "k3s-main"
+  }
+
+  taint {
+    key    = "node-role.kubernetes.io/master"
+    value  = "true"
+    effect = "NoSchedule"
+  }
+}
