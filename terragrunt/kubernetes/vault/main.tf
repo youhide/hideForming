168dc9dfebc158060002662113c61b43c1422016
Youri Theodora Kopoulos Kirchner Mattar <youri@youhide.com.br>
2025-10-24 22:41:22 -0300

    chore(deps): update helm release external-secrets to v0.20.4 (#75)
    Co-authored-by: YouHide Renovate Bot <youhide_renovate@none.com>

--- terragrunt/kubernetes/vault/main.tf
+++ terragrunt/kubernetes/vault/main.tf
@@ -11,7 +11,7 @@ resource "helm_release" "external_secrets" {
   name       = "external-secrets"
   repository = "https://charts.external-secrets.io"
   chart      = "external-secrets"
-  version    = "0.20.3"
+  version    = "0.20.4"
   namespace  = kubernetes_namespace.external_secrets.metadata[0].name
   timeout    = 600
 
