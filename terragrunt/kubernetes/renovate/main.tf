8f0e0b97508c8cf6de52858b2b93e6934a1994d2
Youri Theodora Kopoulos Kirchner Mattar <youri@youhide.com.br>
2025-12-04 23:20:43 -0300

    chore(deps): update helm release renovate to v45.33.1 (#84)
    Co-authored-by: YouHide Renovate Bot <youhide_renovate@none.com>

--- terragrunt/kubernetes/renovate/main.tf
+++ terragrunt/kubernetes/renovate/main.tf
@@ -47,7 +47,7 @@ resource "helm_release" "renovate" {
   name       = "renovate"
   repository = "https://renovatebot.github.io/helm-charts"
   chart      = "renovate"
-  version    = "45.31.0"
+  version    = "45.33.1"
   namespace  = kubernetes_namespace.renovate.metadata[0].name
   timeout    = 600
 
